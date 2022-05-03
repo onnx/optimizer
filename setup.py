792bc0bf04b56b62a4e361ce072522f1331cfb68
daquexian <daquexian566@gmail.com>
2022-05-03 15:46:00 +0800

    fix findpython
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- setup.py
+++ setup.py
@@ -160,8 +160,8 @@ class cmake_build(setuptools.Command):
             # configure
             cmake_args = [
                 CMAKE,
-                '-DPYTHON_INCLUDE_DIR={}'.format(sysconfig.get_python_inc()),
-                '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+                '-DPython_INCLUDE_DIR={}'.format(sysconfig.get_python_inc()),
+                '-DPython_EXECUTABLE={}'.format(sys.executable),
                 '-DBUILD_ONNX_PYTHON=ON',
                 '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
                 '-DONNX_NAMESPACE={}'.format(ONNX_NAMESPACE),
