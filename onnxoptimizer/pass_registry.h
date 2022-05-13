6df3c9302b1d93544f1ce504ecd091184f78a8de
15711303206 <15711303206@163.com>
2022-05-13 03:01:07 +0000

    fuse pad into avgpool and maxpool
    Signed-off-by: 15711303206 <15711303206@163.com>

--- onnxoptimizer/pass_registry.h
+++ onnxoptimizer/pass_registry.h
@@ -33,6 +33,8 @@
 #include "onnxoptimizer/passes/fuse_consecutive_transposes.h"
 #include "onnxoptimizer/passes/fuse_matmul_add_bias_into_gemm.h"
 #include "onnxoptimizer/passes/fuse_pad_into_conv.h"
+#include "onnxoptimizer/passes/fuse_pad_into_avgpool.h"
+#include "onnxoptimizer/passes/fuse_pad_into_maxpool.h"
 #include "onnxoptimizer/passes/fuse_transpose_into_gemm.h"
 #include "onnxoptimizer/passes/lift_lexical_references.h"
 #include "onnxoptimizer/passes/nop.h"
@@ -72,6 +74,8 @@ struct GlobalPassRegistry {
     registerPass<FuseConsecutiveTransposes>();
     registerPass<FuseMatMulAddBiasIntoGemm>();
     registerPass<FusePadIntoConv>();
+    registerPass<FusePadIntoAvgPool>();
+    registerPass<FusePadIntoMaxPool>();
     registerPass<FuseTransposeIntoGemm>();
     registerPass<LiftLexicalReferences>();
     registerPass<SplitInit>();
