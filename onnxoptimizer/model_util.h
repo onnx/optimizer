979bda13af4f1917086a55f9ff8600fb9f3dc72e
haoshengqiang <haoshengqiang79@163.com>
2022-06-28 11:28:01 +0800

    format code
    Signed-off-by: haoshengqiang <haoshengqiang79@163.com>

--- onnxoptimizer/model_util.h
+++ onnxoptimizer/model_util.h
@@ -12,9 +12,12 @@
 namespace ONNX_NAMESPACE {
 namespace optimization {
 
-void loadModel(ModelProto* m, const std::string& model_path,const bool load_external_data = false);
+void loadModel(ModelProto* m, const std::string& model_path,
+               const bool load_external_data = false);
 
-void saveModel(ModelProto*m, const std::string& model_path,const bool save_external_data = false, const std::string& data_file_name = {});
+void saveModel(ModelProto* m, const std::string& model_path,
+               const bool save_external_data = false,
+               const std::string& data_file_name = {});
 
 }  // namespace optimization
 }  // namespace ONNX_NAMESPACE
