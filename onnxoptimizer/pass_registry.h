a4a71050a21398b28ecb1d78d7040999b06d825b
sqhao <haoshengqiang79@163.com>
2023-02-10 01:07:42 +0800

    adjust add pass (#111)
    Signed-off-by: haoshengqiang <haoshengqiang79@163.com>

--- onnxoptimizer/pass_registry.h
+++ onnxoptimizer/pass_registry.h
@@ -51,6 +51,7 @@
 #include "onnxoptimizer/passes/replace_einsum_with_matmul.h"
 #include "onnxoptimizer/passes/eliminate_nop_concat.h"
 #include "onnxoptimizer/passes/eliminate_nop_split.h"
+#include "onnxoptimizer/passes/adjust_add.h"
 
 namespace ONNX_NAMESPACE {
 namespace optimization {
@@ -62,6 +63,7 @@ struct GlobalPassRegistry {
 
   GlobalPassRegistry() {
     // Register the optimization passes to the optimizer.
+    registerPass<AdjustAdd>();
     registerPass<RenameInputOutput>();
     registerPass<SetUniqueNameForNodes>();
     registerPass<NopEmptyPass>();
