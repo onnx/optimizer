b9f0a9daf10333423dc53c0b1317f38bda87b2c7
Andreas Fehlner <fehlner@arcor.de>
2025-05-23 15:50:30 +0200

    Update setup.py
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- setup.py
+++ setup.py
@@ -327,7 +327,7 @@ long_description = (this_directory / "README.md").read_text()
 setuptools.setup(
     name="onnxoptimizer",
     version=VersionInfo.version,
-    description="Actively maintained ONNX Optimizer",
+    description="ONNX Optimizer",
     ext_modules=ext_modules,
     cmdclass=cmdclass,
     packages=packages,
