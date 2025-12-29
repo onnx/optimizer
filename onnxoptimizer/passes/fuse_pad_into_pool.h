efbca62274e41acd48ced1780a8ab0d38ae8156c
Andreas Fehlner <fehlner@arcor.de>
2025-12-29 09:38:44 +0100

    add copyright license (#223)
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- onnxoptimizer/passes/fuse_pad_into_pool.h
+++ onnxoptimizer/passes/fuse_pad_into_pool.h
@@ -1,6 +1,6 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright (c) ONNX Project Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
 
 // ATTENTION: The code in this file is highly EXPERIMENTAL.
 // Adventurous users should note that the APIs will probably change.
