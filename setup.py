5b532f0fd59d470b1c88c25983466ead63eecda7
Justin Chu <justinchuby@users.noreply.github.com>
2025-12-29 22:14:16 -0800

    Remove unnecessary future imports from setup.py
    Removed future imports that are no longer necessary.
    
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 # Copyright (c) ONNX Project Contributors
 #
 # SPDX-License-Identifier: Apache-2.0
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from distutils import sysconfig, log
 import setuptools
 import setuptools.command.build_py
