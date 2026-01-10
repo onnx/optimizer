80e15eb7e211b0a36c1dd3a7e704606680511802
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-10 12:59:12 -0800

    No preview rules
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxoptimizer/__init__.py
+++ onnxoptimizer/__init__.py
@@ -5,48 +5,56 @@
 # ATTENTION: The code in this file is highly EXPERIMENTAL.
 # Adventurous users should note that the APIs will probably change.
 
-"""onnx optimizer
+"""ONNX optimizer."""
 
-This enables users to optimize their models.
-"""
+from __future__ import annotations
+
+__all__ = [
+    "optimize",
+    "get_available_passes",
+    "get_fuse_and_elimination_passes",
+    "main",
+]
+
 
 import os
 import tempfile
 
 import onnx
-from onnx import ModelProto
 
-import onnxoptimizer.onnx_opt_cpp2py_export as onnx_opt_cpp2py_export
+import onnxoptimizer.onnx_opt_cpp2py_export as _c
+from onnxoptimizer.onnx_opt_cpp2py_export import (
+    get_available_passes,
+    get_fuse_and_elimination_passes,
+)
 from onnxoptimizer.onnxoptimizer_main import main
 
 from .version import version as __version__  # noqa
 
-get_available_passes = onnx_opt_cpp2py_export.get_available_passes
 
-get_fuse_and_elimination_passes = onnx_opt_cpp2py_export.get_fuse_and_elimination_passes
-
-
-def optimize(model, passes=None, fixed_point=False):  # type: (ModelProto, Optional[Sequence[Text]], bool) -> ModelProto
+def optimize(
+    model: onnx.ModelProto, passes: list[str] | None = None, fixed_point: bool = False
+) -> onnx.ModelProto:
     """Apply the optimization on the serialized ModelProto.
 
     Arguments:
-        model (ModelProto): model
-        passes (list of string): list of optimization names
+        model: ONNX model.
+        passes: Optimization names.
 
     Return:
-        return (ModelProto) optimized model
+        Optimized model.
     """
 
     if passes is None:
         passes = get_fuse_and_elimination_passes()
-    if not isinstance(model, ModelProto):
+    if not isinstance(model, onnx.ModelProto):
         raise TypeError(f"Optimizer only accepts ModelProto, incorrect type: {type(model)}")
     try:
         model_str = model.SerializeToString()
         if fixed_point:
-            optimized_model_str = onnx_opt_cpp2py_export.optimize_fixedpoint(model_str, passes)
+            optimized_model_str = _c.optimize_fixedpoint(model_str, passes)
         else:
-            optimized_model_str = onnx_opt_cpp2py_export.optimize(model_str, passes)
+            optimized_model_str = _c.optimize(model_str, passes)
 
         return onnx.load_from_string(optimized_model_str)
     except ValueError:
@@ -71,11 +79,11 @@ def optimize(model, passes=None, fixed_point=False):  # type: (ModelProto, Optio
                     convert_attribute=True,
                 )
                 if fixed_point:
-                    onnx_opt_cpp2py_export.optimize_fixedpoint_from_path(
+                    _c.optimize_fixedpoint_from_path(
                         file_src.name, file_dest.name, passes, data_dest_rel_filename
                     )
                 else:
-                    onnx_opt_cpp2py_export.optimize_from_path(
+                    _c.optimize_from_path(
                         file_src.name, file_dest.name, passes, data_dest_rel_filename
                     )
                 return onnx.load(file_dest.name, load_external_data=True)
@@ -84,6 +92,3 @@ def optimize(model, passes=None, fixed_point=False):  # type: (ModelProto, Optio
                 os.remove(file_dest.name)
                 os.remove(data_file_src.name)
                 os.remove(data_file_dest.name)
-
-
-__all__ = ["optimize", "get_available_passes", "get_fuse_and_elimination_passes", "main"]
