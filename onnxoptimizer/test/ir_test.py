bb1c6a4a100cfdd1ab5881fd11d2baf51b761b5c
take-cheeze <takechi101010@gmail.com>
2026-01-22 19:53:26 +0900

    Fix type information vanishing for non tensor types
    Signed-off-by: take-cheeze <takechi101010@gmail.com>

---
+++ onnxoptimizer/test/ir_test.py
@@ -0,0 +1,8 @@
+import onnx
+import onnx.backend.test.loader
+import onnxoptimizer
+
+def test_seq_type():
+    orig = onnx.load(onnx.backend.test.loader.DATA_DIR + "/node/test_sequence_insert_at_back/model.onnx")
+    optimized = onnxoptimizer.optimize(orig)
+    assert optimized.graph.input[0].type == orig.graph.input[0].type
